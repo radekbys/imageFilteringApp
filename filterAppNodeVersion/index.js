c30af55c226d9206bff2f4988867338181e2f41f
radekbys <radekbry12@gmail.com>
2023-09-30 17:51:20 +0200

    finished with simple filters

--- filterAppNodeVersion/index.js
+++ filterAppNodeVersion/index.js
@@ -1,4 +1,5 @@
 const Jimp = require('jimp');
+const sanitize = require('sanitize-filename');
 const { Filter } = require('./utils/Filter');
 const { lowPassFilters } = require('./filters/lowPassFilters');
 const { highPassFilters } = require('./filters/highPassFilters ');
@@ -6,18 +7,37 @@ const { embossingFilters } = require('./filters/embossingFilters');
 const { laplaceFilters } = require('./filters/laplaceFilters');
 const { contourFilters } = require('./filters/contourFilters');
 
-// console.log(process.argv);
+// check if the filter is in the filters object
+// if it is, return the filter
+const getFilterIfInObject = (filterName, filtersObject) => {
+  if (Object.keys(filtersObject).includes(filterName)) {
+    return filtersObject[filterName];
+  }
+  return null;
+};
 
-(async () => {
-  const bird = await Jimp.read('../images/bird.jpg');
-  const time = Date.now();
-  const filteredBuffer = Filter.simpleFilter(
-    bird.bitmap.data,
-    bird.bitmap.width,
-    bird.bitmap.height,
-    contourFilters.verticalSobelFilter,
-  );
-  console.log(Date.now() - time);
-  bird.bitmap.data = filteredBuffer;
-  bird.write('../images/output.jpg');
-})();
+// check filters objects for searched filter
+const usedSimpleFilter = getFilterIfInObject(process.argv[2], lowPassFilters)
+|| getFilterIfInObject(process.argv[2], highPassFilters)
+|| getFilterIfInObject(process.argv[2], embossingFilters)
+|| getFilterIfInObject(process.argv[2], laplaceFilters)
+|| getFilterIfInObject(process.argv[2], contourFilters);
+
+const inputFile = `../images/${sanitize(process.argv[3])}`;
+const outputFile = `../images/${sanitize(process.argv[4])}`;
+
+if (usedSimpleFilter) {
+  (async () => {
+    const bird = await Jimp.read(inputFile);
+    // const time = Date.now();
+    const filteredBuffer = Filter.simpleFilter(
+      bird.bitmap.data,
+      bird.bitmap.width,
+      bird.bitmap.height,
+      usedSimpleFilter,
+    );
+    // console.log(Date.now() - time);
+    bird.bitmap.data = filteredBuffer;
+    bird.write(outputFile);
+  })();
+}
