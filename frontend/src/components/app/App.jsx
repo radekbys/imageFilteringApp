5b27e4cb9adc6b4e66a2ddfb6334b49971f978a1
radekbys <radekbry12@gmail.com>
2023-10-04 19:23:21 +0200

    started doing some basic frontend

---
+++ frontend/src/components/app/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Nav from '../nav/Nav';
+import Footer from '../footer/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <div className="footer">
+      <small>Written by Radosław Bryś, open source do whatever you want</small>
+    </div>
+  );
+}
+
+export default Footer;
