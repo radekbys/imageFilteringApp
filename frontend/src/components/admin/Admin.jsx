95f494e9d5aef35a9fe01f69f9f5e129394b4de6
radekbys <radekbry12@gmail.com>
2023-10-09 22:08:57 +0200

    Added forms to the admin panel, started scripting them.

--- frontend/src/components/admin/Admin.jsx
+++ frontend/src/components/admin/Admin.jsx
@@ -1,9 +1,32 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import './Admin.css';
+import NewUserForm from '../newUserForm/NewUserForm';
 
 function Admin() {
   return (
-    <h1>Admin</h1>
+    <div className="admin-panel">
+      <NewUserForm />
+      <br />
+      <form className="remove-user-form">
+        <h2 className="form-header">Remove User:</h2>
+        <div>
+          <label className="form-label" htmlFor="form-input-removed">{'Username: '}</label>
+          <input
+            type="text"
+            id="form-input-removed"
+            name="formRemovedUser"
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      <div className="admin-all-users">
+        <h2>All Users:</h2>
+        <p>username</p>
+        <p>Email</p>
+        <p>isAdmin</p>
+      </div>
+    </div>
   );
 }
 
---
+++ frontend/src/components/newUserForm/NewUserForm.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import './NewUserForm.css';
+
+function NewUserForm() {
+// eslint-disable-next-line no-unused-vars
+  const [newUser, setNewUser] = useState({
+    newUsername: '',
+    newEmail: '',
+    newAdmin: false,
+    newPassword: '',
+  });
+
+  const updateNewUser = (event) => {
+    const {
+      name, value, type, checked,
+    } = event.target;
+    setNewUser({
+      ...newUser,
+      [name]: type === 'checkbox' ? checked : value,
+    });
+  };
+
+  const submitNewUser = (event) => {
+    event.preventDefault();
+    console.log(newUser);
+    setNewUser({
+      newUsername: '',
+      newEmail: '',
+      newAdmin: false,
+      newPassword: '',
+    });
+  };
+
+  return (
+    <form className="add-user-form" onSubmit={submitNewUser}>
+      <h2 className="form-header">Add new User:</h2>
+      <div>
+        <label className="form-label" htmlFor="form-input-username">{'New user\'s username: '}</label>
+        <input
+          onChange={updateNewUser}
+          type="text"
+          id="form-input-username"
+          name="newUsername"
+          value={newUser.newUsername}
+        />
+      </div>
+      <div>
+        <label className="form-label" htmlFor="form-input-email">{'New user\'s Email: '}</label>
+        <input
+          onChange={updateNewUser}
+          type="email"
+          id="form-input-email"
+          name="newEmail"
+          value={newUser.newEmail}
+        />
+      </div>
+      <div>
+        <label className="form-label" htmlFor="form-input-admin">{'Give Admin role: '}</label>
+        <input
+          onChange={updateNewUser}
+          type="checkbox"
+          id="form-input-admin"
+          name="newAdmin"
+          checked={newUser.newAdmin}
+        />
+      </div>
+      <div>
+        <label className="form-label" htmlFor="form-input-password">{'New user\'s password: '}</label>
+        <input
+          onChange={updateNewUser}
+          type="text"
+          id="form-input-password"
+          name="newPassword"
+          value={newUser.newPassword}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default NewUserForm;
